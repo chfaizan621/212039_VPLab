bc479a31fd5ac3325b2ed60644cf0e3edd4ae290
M.Faizan <M.Faizan@DESKTOP-8I9IBNF>
2023-09-24 16:01:13 +0500

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1.Task1_and_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Person person1 = new Person("Faizan", 20);
+
+            Console.WriteLine("Task 1: ");
+            Console.WriteLine("Name: " + person1.GetName());
+            Console.WriteLine("Age: " + person1.GetAge());
+
+            Console.WriteLine("\nTask 2: ");
+            Person person2 = new Person("M.Faizan");
+            Person person3 = new Person(19);
+
+            Console.WriteLine("Name: " + person2.GetName());
+            Console.WriteLine("Age: " + person3.GetAge());
+
+
+        }
+    }
+}
---
+++ Lab1/Task1 and Task2/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Task1_and_Task2
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            name = string.Empty;
+        }
+
+        public Person(string n, int a)
+        {
+            name = n;
+            age = a;
+        }
+        public Person(string n)
+        {
+            name = n;
+        }
+
+        public Person(int a)
+        {
+            age = a;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+    }
+}
